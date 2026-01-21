7f9ee7e570b4b35dc4e6baa53090ad984a42473e
anpifini06102-pixelecho <anpifini06102@gmail.com>
2026-01-22 00:00:00 +0800

    Setup GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/one-year-harvests/',
     plugins: [react()],
     build: {
         target: 'es2015'
